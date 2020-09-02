502c618cb3cd253b67dd5259c0bf1f4571111d4b
preciouStar <seongjin605@users.noreply.github.com>
2020-09-03 00:15:42 +0900

    Add layout

---
+++ src/layout/Specific.tsx
@@ -0,0 +1,22 @@
+import Header from './Header';
+
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #FF385C'
+};
+
+interface Props {
+  name: string;
+}
+
+const Layout: React.FC<Props> = props => {
+  return (
+    <div style={layoutStyle}>
+      <Header />
+      {props.name}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../layout/Specific';
+
+const Index: React.FC = () => (
+  <div>
+    <Layout name={'test'}>
+      <p>Hello next</p>
+    </Layout>
+  </div>
+);
+
+export default Index;
