c3941ddb7f76cc3e6673be0f6dd5ecec34f4f8e4
preciouStar <seongjin605@users.noreply.github.com>
2020-09-08 00:49:18 +0900

    dynmaic request and components rendering

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,16 +1,91 @@
-import React from 'react';
-import Layout from '../layout/Specific';
-import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import axios from 'axios';
 
-const Posts: React.FC = () => {
-  const router = useRouter();
-  console.log('router:', router);
-  console.log('query:', router.query);
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      maxWidth: 345
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%' // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest
+      })
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)'
+    },
+    avatar: {
+      backgroundColor: red[500]
+    }
+  })
+);
+
+interface Movie {
+  title: string;
+}
+
+export default function RecipeReviewCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const [movies, setMovies] = React.useState<Array<Movie>>([]);
+
+  async function getMovies() {
+    try {
+      const {
+        data: {
+          data: { movies = [] }
+        }
+      }: { data: { data: { movies: Array<Movie> } } } = await axios.get('/api');
+
+      console.log('movies:', movies);
+      setMovies(movies);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    if (Array.isArray(movies) && movies.length === 0) {
+      getMovies();
+    }
+  });
+
+  if (movies.length === 0) {
+    return <h1>로딩....</h1>;
+  }
+  console.log('로딩 끝');
   return (
-    <div>
-      <Layout name="Specific Layout" query={router.query.title} />
-    </div>
+    <ul>
+      {movies.map((movie, key) => (
+        <li key={key}>{movie.title}</li>
+      ))}
+    </ul>
   );
-};
-
-export default Posts;
+}
