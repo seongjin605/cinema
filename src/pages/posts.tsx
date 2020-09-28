47b698a1be3982823c804ec8fec4a03ae8673d0a
preciouStar <seongjin605@users.noreply.github.com>
2020-09-28 20:51:25 +0900

    Add loading skeleton

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -2,38 +2,42 @@ import React, { useEffect } from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 import axios from 'axios';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      maxWidth: 345
+    card: {
+      maxWidth: 345,
+      margin: theme.spacing(2)
     },
     media: {
-      height: 0,
-      paddingTop: '56.25%' // 16:9
-    },
-    expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest
-      })
-    },
-    expandOpen: {
-      transform: 'rotate(180deg)'
-    },
-    avatar: {
-      backgroundColor: red[500]
+      height: 190
     }
   })
 );
 
-interface Movie {
+// interface Movie {
+//   title: string;
+//   loading?: false;
+// }
+
+interface MovieProps {
   title: string;
+  loading?: false;
 }
 
-export default function RecipeReviewCard() {
-  const [movies, setMovies] = React.useState<Array<Movie>>([]);
+export default function RecipeReviewCard(props: MovieProps) {
+  const [movies, setMovies] = React.useState<Array<MovieProps>>([]);
+  const { loading = false } = props;
+  const classes = useStyles();
 
   async function getMovies() {
     try {
@@ -41,9 +45,8 @@ export default function RecipeReviewCard() {
         data: {
           data: { movies = [] }
         }
-      }: { data: { data: { movies: Array<Movie> } } } = await axios.get('/api');
+      }: { data: { data: { movies: Array<MovieProps> } } } = await axios.get('/api');
 
-      console.log('movies:', movies);
       setMovies(movies);
     } catch (error) {
       console.error(error);
@@ -56,16 +59,62 @@ export default function RecipeReviewCard() {
     }
   });
 
-  if (movies.length === 0) {
-    return <h1>로딩....</h1>;
-  }
+  // if (movies.length === 0) {
+  //   return <h1>로딩....</h1>;
+  // }
   console.log('로딩 끝');
-  // TODO: Skeleton Loaders
   return (
-    <ul>
-      {movies.map((movie, key) => (
-        <li key={key}>{movie.title}</li>
-      ))}
-    </ul>
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          loading ? (
+            <Skeleton animation="wave" variant="circle" width={40} height={40} />
+          ) : (
+            <Avatar
+              alt="Ted talk"
+              src="https://pbs.twimg.com/profile_images/877631054525472768/Xp5FAPD5_reasonably_small.jpg"
+            />
+          )
+        }
+        action={
+          loading ? null : (
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          )
+        }
+        title={
+          loading ? (
+            <Skeleton animation="wave" height={10} width="80%" style={{ marginBottom: 6 }} />
+          ) : (
+            'Ted'
+          )
+        }
+        subheader={loading ? <Skeleton animation="wave" height={10} width="40%" /> : '5 hours ago'}
+      />
+      {loading ? (
+        <Skeleton animation="wave" variant="rect" className={classes.media} />
+      ) : (
+        <CardMedia
+          className={classes.media}
+          image="https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/72bda89f-9bbf-4685-910a-2f151c4f3a8a/NicolaSturgeon_2019T-embed.jpg?w=512"
+          title="Ted talk"
+        />
+      )}
+      <CardContent>
+        {loading ? (
+          <React.Fragment>
+            <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
+            <Skeleton animation="wave" height={10} width="80%" />
+          </React.Fragment>
+        ) : (
+          <Typography variant="body2" color="textSecondary" component="p">
+            {
+              "Why First Minister of Scotland Nicola Sturgeon thinks GDP is the wrong measure of a country's success:"
+            }
+          </Typography>
+        )}
+      </CardContent>
+    </Card>
   );
 }
