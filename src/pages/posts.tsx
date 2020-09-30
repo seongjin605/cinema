1ff803b8d4ceb1baa59b725bc46801cccd82acfe
preciouStar <seongjin605@users.noreply.github.com>
2020-09-30 20:37:55 +0900

    Update icon

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
 import axios from 'axios';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
