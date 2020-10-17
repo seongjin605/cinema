f8ddea4615a62fbc5b15e3d6decae84e6851491f
preciouStar <seongjin605@users.noreply.github.com>
2020-10-17 10:31:06 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import Layout from '../layout/Layout';
+import Head from 'next/head';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: auto;
+`;
 
 const Index = () => {
-  return <Layout></Layout>;
+  return (
+    <Container>
+      <Layout />
+      <Head>
+        <title>Welcome to cinema!</title>
+      </Head>
+    </Container>
+  );
 };
 
 export default Index;
